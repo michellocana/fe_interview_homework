aea6a035ca4b916557f71ae82ec8c69ca5bbc749
Michell Ocaña <michell.santo@cwi.com.br>
2024-02-22 12:33:14 -0300

    feat(responsiveness): fix Select icon pointer-events

--- src/lib/components/UI/Select.svelte
+++ src/lib/components/UI/Select.svelte
@@ -22,7 +22,7 @@
     viewBox="0 0 24 24"
     stroke-width="1.5"
     stroke="currentColor"
-    class="absolute bottom-0 right-1 top-0 m-auto h-4"
+    class="pointer-events-none absolute bottom-0 right-1 top-0 m-auto h-4"
   >
     <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
   </svg>
